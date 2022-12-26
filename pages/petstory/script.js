0e8a4aac7ca023a8411608a47fb52faddacdc8fd
Artem <artemmakarov76@yandex.ru>
2022-12-26 19:41:23 +0300

    feat: make structure of picking and make picking demo element

--- pages/petstory/script.js
+++ pages/petstory/script.js
@@ -8,3 +8,27 @@ burger.addEventListener("click", function(){
   menu.classList.toggle("active");
   menu__panel.classList.toggle("active");
 })
+
+let subscriptionInput = document.querySelector(".subscription__input");
+let subscriptionButton = document.querySelector(".send-button");
+let inputContainer = document.querySelector(".input__container");
+
+subscriptionInput.addEventListener("focus", function(){
+  subscriptionButton.classList.add("active");
+  inputContainer.classList.add("active");
+})
+
+subscriptionInput.addEventListener("blur", function(){
+  subscriptionButton.classList.remove("active");
+  inputContainer.classList.remove("active");
+  subscriptionButton.classList.remove("error");
+  subscriptionInput.classList.remove("error");
+  inputContainer.classList.remove("error");
+})
+
+subscriptionInput.addEventListener("invalid", function(){
+  subscriptionButton.classList.remove("active");
+  subscriptionButton.classList.add("error");
+  subscriptionInput.classList.add("error");
+  inputContainer.classList.add("error");
+})
