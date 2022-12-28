28687bafe24d666a239686bb07cada061e4fc1db
Artem <artemmakarov76@yandex.ru>
2022-12-28 20:35:20 +0300

    feat: make option-bar

--- pages/donate/script.js
+++ pages/donate/script.js
@@ -8,3 +8,19 @@ enterInput.addEventListener("focus", function(){
 enterInput.addEventListener("blur", function(){
   enterField.classList.remove("active");
 })
+
+
+let inputRange = document.querySelector(".scroll");
+let amountList = document.querySelector(".option-bar__amount");
+
+
+inputRange.addEventListener("change", function(){
+
+  for (let i =0; i < amountList.children.length; i++) {
+    amountList.children[i].classList.remove("active");
+  }
+  
+  amountList.children[inputRange.value-1].classList.add("active");
+  let amount = amountList.children[inputRange.value-1].textContent;
+  enterInput.value = amount;
+})
